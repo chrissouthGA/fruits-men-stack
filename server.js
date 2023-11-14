7cf30a5f4eb6ac03cfdff26641570c4c48dc3a95
Gr8ness <paul.chapman@generalassemb.ly>
2023-11-13 20:51:47 -0500

    Update server.js
    Updated Server.js

--- server.js
+++ server.js
@@ -3,17 +3,38 @@ const app = express()
 const mongoose = require('mongoose')
 const Fruit = require('./models/fruits.js')
 
+// DATABASE CONNECTION
 mongoose.connect('mongodb://127.0.0.1:27017/basiccrud')
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo')
 })
 
+// MIDDLEWARE
 app.use(express.urlencoded({extended:true}))
 
+// ROUTES (I.N.D.U.C.E.S.)
+
+// INDEX
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('index.ejs', {
+            fruits: allFruits
+        })
+    })
+})
+
+// NEW
 app.get('/fruits/new', (req, res) => {
     res.render('new.ejs')
 })
 
+// DELETE
+
+
+// UPDATE
+
+
+// CREATE
 app.post('/fruits', (req, res) => {
     if(req.body.readyToEat === 'on') {
         req.body.readyToEat = true;
@@ -26,8 +47,20 @@ app.post('/fruits', (req, res) => {
             console.log(error)
             res.send(error)
         } else {
-            res.send(createdFruit)
+            res.redirect('/fruits')
         }
+    })
+})
+
+// EDIT
+
+
+// SHOW
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('show.ejs', {
+            fruit: foundFruit
+        })
     })
 })
 
