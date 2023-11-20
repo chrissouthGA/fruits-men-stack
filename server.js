915392ad92fc7344769e14e8f9c3c24a0ddc1433
chrissouth89 <chrissouth89@gmail.com>
2023-11-20 14:01:47 -0800

    added home route

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// required dependencies
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const methodOverride = require('method-override');
+// controller variables
 const fruitsController = require('./controllers/fruits');
-require('dotenv').config()
+// env variables
 const mongoUrl = process.env.MONGOURI
 const PORT = process.env.PORT
 
@@ -17,7 +20,14 @@ mongoose.connection.once('open', ()=> {
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(methodOverride('_method'))
+
+// controller middleware
 app.use('/fruits', fruitsController);
+
+// routes
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
 
 app.listen(PORT, ()=> {
     console.log(`listening on port ${PORT}`)
